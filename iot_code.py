c2776ed774852c633b206bbae06c97c3eaf386a3
jam-adagni <shamajamadagni5@gmail.com>
2026-03-03 15:52:18 +0530

    Update iot_code.py

--- iot_code.py
+++ iot_code.py
@@ -88,11 +88,11 @@ def store_iot(rows):
         return
 
     conn = psycopg2.connect(
-        host=os.getenv("DB_HOST"),
-        port=os.getenv("DB_PORT"),
-        dbname=os.getenv("CVE_DB_NAME"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD")
+            host=os.getenv('DB_HOST', 'localhost'),
+            port=os.getenv('DB_PORT', '5432'),
+            dbname=os.getenv('CVE_DB_NAME', 'Baseel_CVE'),
+            user=os.getenv('DB_USER', 'postgres'),
+            password=os.getenv('DB_PASSWORD')
     )
 
     cur = conn.cursor()
